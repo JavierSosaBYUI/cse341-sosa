4b32c3e2cc9bcc4486a6665723b3b09f37c01da8
JAVIER SOSA <sos17001@byui.edu>
2025-09-19 22:54:27 -0400

    last_changes

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const getName = (req, res) => {
+  res.send('Marcia Vallejos');
+};
+
+module.exports = { getName };
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Javier Sosa');
-});
+app.use('/', require('./routes'))
 
-const port = 3000;
-
-app.listen(process.env.port  || port);
-console.log('Web Server is listening at port' + (process.env.port || port));
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
