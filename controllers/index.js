c482dedca35f18af253602ccb7c1d17e294d51c1
JAVIER SOSA <sos17001@byui.edu>
2025-09-23 23:51:43 -0400

    L02 Personal Assignment

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-  res.json('Marcia Vallejos');
-};
-
-const returnAnotherpPerson = (req, res, next) => {
-  res.json('Mon Coeur');
-}
-
-module.exports = { awesomeFunction, returnAnotherpPerson };
